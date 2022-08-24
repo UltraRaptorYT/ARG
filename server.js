1ca3c571c7cfc5cfacc838429716a27f2e0141f5
UltraRaptorYT <62703912+UltraRaptorYT@users.noreply.github.com>
2022-08-25 01:31:17 +0800

    Prepare project

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const path = require("path");
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(cors());
+
+app.use(express.static("client/public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "public", "error.html"));
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
