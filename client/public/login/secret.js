2ee95a255d71ae566381dd4f1de4c448aec13807
gatari <79693291+deceptivecz@users.noreply.github.com>
2022-12-18 00:23:56 +0800

    final commit i promise

--- client/public/login/secret.js
+++ client/public/login/secret.js
@@ -1,5 +1,5 @@
 const p = document.createElement('h3');
-p.style.marginTop = "20px"; // There has to be a better way to do this
+p.style.marginTop = "20px";
 document.addEventListener("DOMContentLoaded", () => {
   const hashedPassword = '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08'
   const passwordInput = document.getElementById("password-input");
