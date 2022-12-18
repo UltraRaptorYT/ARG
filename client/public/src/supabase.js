f57bbcade3602a12b21080d1813c6a361e60ac65
Acrylic125 <acrylic125email@gmail.com>
2022-12-18 12:02:03 +0800

    feat: sign up page

---
+++ client/public/src/supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://lvixgdrkzecihyzpessj.supabase.co";
+const SUPABASE_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx2aXhnZHJremVjaWh5enBlc3NqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzA4OTI3MjEsImV4cCI6MTk4NjQ2ODcyMX0.o8MGdLzgvOeQCRyL0uFkldN2wwlOBWZtx8SUBY29OlY";
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export default client;
