7381fd908eb7dc990a34d42911dae8014d71c8b9
Soh Hong Yu <sohhongyu@gmail.com>
2022-12-25 02:27:26 +0800

    update UI

--- client/public/src/chat.js
+++ client/public/src/chat.js
@@ -1,112 +1,34 @@
-// MESSAGE INPUT
-const textarea = document.querySelector('.chatbox-message-input')
-const chatboxForm = document.querySelector('.chatbox-message-form')
+var chat = document.getElementById("chat");
 
-textarea.addEventListener('input', function () {
-	let line = textarea.value.split('\n').length
+document.querySelector("#message").addEventListener("keydown", (e) => {
+  if (e.key == "Enter") {
+    writeMessage();
+  }
+});
 
-	if(textarea.rows < 6 || line < 6) {
-		textarea.rows = line
-	}
-
-	if(textarea.rows > 1) {
-		chatboxForm.style.alignItems = 'flex-end'
-	} else {
-		chatboxForm.style.alignItems = 'center'
-	}
-})
-
-
-
-// TOGGLE CHATBOX
-// const chatboxToggle = document.querySelector('.chatbox-toggle')
-const chatboxMessage = document.querySelector('.chatbox-message-wrapper')
-
-// chatboxToggle.addEventListener('click', function () {
-// 	chatboxMessage.classList.toggle('show')
-// })
-
-
-
-// DROPDOWN TOGGLE
-const dropdownToggle = document.querySelector('.chatbox-message-dropdown-toggle')
-const dropdownMenu = document.querySelector('.chatbox-message-dropdown-menu')
-
-dropdownToggle.addEventListener('click', function () {
-	dropdownMenu.classList.toggle('show')
-})
-
-document.addEventListener('click', function (e) {
-	if(!e.target.matches('.chatbox-message-dropdown, .chatbox-message-dropdown *')) {
-		dropdownMenu.classList.remove('show')
-	}
-})
-
-
-
-
-
-
-
-// CHATBOX MESSAGE
-const chatboxMessageWrapper = document.querySelector('.chatbox-message-content')
-const chatboxNoMessage = document.querySelector('.chatbox-message-no-message')
-
-chatboxForm.addEventListener('submit', function (e) {
-	e.preventDefault()
-
-	if(isValid(textarea.value)) {
-		writeMessage()
-		setTimeout(autoReply, 1000)
-	}
-})
-
-
-
-function addZero(num) {
-	return num < 10 ? '0'+num : num
-}
+document.querySelector("#send").addEventListener("click", () => {
+  writeMessage();
+});
 
 function writeMessage() {
-	const today = new Date()
-	let message = `
-		<div class="chatbox-message-item sent">
-			<span class="chatbox-message-item-text">
-				${textarea.value.trim().replace(/\n/g, '<br>\n')}
-			</span>
-			<span class="chatbox-message-item-time">${addZero(today.getHours())}:${addZero(today.getMinutes())}</span>
-		</div>
-	`
-	chatboxMessageWrapper.insertAdjacentHTML('beforeend', message)
-	chatboxForm.style.alignItems = 'center'
-	textarea.rows = 1
-	textarea.focus()
-	textarea.value = ''
-	chatboxNoMessage.style.display = 'none'
-	scrollBottom()
+  var message = document.getElementById("message").value;
+  if (message != "") {
+    chat.innerHTML += `
+			<div class="me bubble">${message}</div>
+		`;
+    scrollBottom();
+    setTimeout(autoReply(), 5000);
+  }
+  document.getElementById("message").value = "";
 }
 
-function autoReply() {
-	const today = new Date()
-	let message = `
-		<div class="chatbox-message-item received">
-			<span class="chatbox-message-item-text">
-				Hi there!
-			</span>
-			<span class="chatbox-message-item-time">${addZero(today.getHours())}:${addZero(today.getMinutes())}</span>
-		</div>
-	`
-	chatboxMessageWrapper.insertAdjacentHTML('beforeend', message)
-	scrollBottom()
+function autoReply(message = "Hello!") {
+  chat.innerHTML += `
+	<div class="other bubble">${message}</div>
+	`;
+  scrollBottom();
 }
 
 function scrollBottom() {
-	chatboxMessageWrapper.scrollTo(0, chatboxMessageWrapper.scrollHeight)
-}
-
-function isValid(value) {
-	let text = value.replace(/\n/g, '')
-	text = text.replace(/\s/g, '')
-
-	return text.length > 0
+  chat.scrollTo(0, chat.scrollHeight);
 }
