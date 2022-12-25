83162fb0e419f725f8b1777b8fe4510dfdbc3570
Soh Hong Yu <sohhongyu@gmail.com>
2022-12-25 16:51:12 +0800

    Add speak to the chatbot page

--- client/public/src/chat.js
+++ client/public/src/chat.js
@@ -22,11 +22,37 @@ function writeMessage() {
   document.getElementById("message").value = "";
 }
 
-function autoReply(message = "Hello!") {
+function autoReply(
+  message = `Hello! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa ex maiores ab deleniti magnam quidem quibusdam consectetur sequi. Ducimus delectus, vitae dolores ipsa labore vel doloremque esse odit blanditiis accusamus!
+    Iusto non fugiat tempora commodi quidem repudiandae, obcaecati id ab eum nemo tenetur molestias nesciunt modi illo? Animi earum perspiciatis nam quae, quod rerum assumenda, non officiis nobis odit sit.
+    Perspiciatis quaerat reprehenderit quasi non dolores? Expedita labore magnam delectus, quos ipsum deleniti inventore amet sequi maiores, debitis dolore iure molestiae aliquam omnis. Delectus, adipisci temporibus laborum eum quia dolor?
+    Accusantium maxime excepturi nemo dolores, porro corporis, quisquam minus cumque, vitae mollitia molestiae iste minima non fugiat quia ipsa? Ad porro culpa ex dolores dolorem voluptatem, aliquid dolor sit voluptas.
+    Facere consequuntur debitis in, laborum sunt dolorum, tempore praesentium dolorem fugiat mollitia laboriosam ut harum assumenda adipisci libero eveniet nostrum numquam, magni iure at. Similique placeat delectus minus unde totam?`
+) {
   chat.innerHTML += `
 	<div class="other bubble">${message}</div>
 	`;
+  playText(message);
   scrollBottom();
+}
+
+const synth = window.speechSynthesis;
+
+function playText(text) {
+  var utterance = new SpeechSynthesisUtterance();
+  if (synth.paused && synth.speaking) {
+    return synth.resume();
+  }
+  if (speechSynthesis.speaking) return;
+  utterance.text = text;
+  utterance.rate = 1;
+  if (mute.dataset.mute == "false") {
+    synth.speak(utterance);
+  }
+}
+
+function stopText() {
+  synth.cancel();
 }
 
 function scrollBottom() {
@@ -44,5 +70,6 @@ mute.addEventListener("click", () => {
     mute.children[0].classList.remove("bi-volume-up-fill");
     mute.children[0].classList.add("bi-volume-mute-fill");
     mute.dataset.mute = "true";
+    stopText();
   }
 });
