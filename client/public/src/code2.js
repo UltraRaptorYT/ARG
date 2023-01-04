d3e3f3d99b58c3114a9b48948060ae7f6c8d2057
Soh Hong Yu <sohhongyu@gmail.com>
2023-01-04 13:49:46 +0800

    Update code.js

--- client/public/src/code2.js
+++ client/public/src/code2.js
@@ -3,13 +3,13 @@ const p = document.createElement("h3");
 p.style.marginTop = "20px";
 document.addEventListener("DOMContentLoaded", () => {
   const hashedPassword =
-    "98f3aaa79f6ba1759e046f873955785d869eec78b60ff7ad2f1bb62d50ea8a0a";
+    "96c32e6230333f3d32a3482cd4a8eb85c88c2f6c4de80788184e9cd5adc4fa43";
   const passwordInput = document.getElementById("password-input");
   const submitButton = document.getElementById("password-submit");
   submitButton.addEventListener("click", (event) => {
     event.preventDefault();
     crypto.subtle
-      .digest("SHA-256", new TextEncoder().encode(passwordInput.value))
+      .digest("SHA-256", new TextEncoder().encode(passwordInput.toUpperCase().value))
       .then(function (hash) {
         const hashedUserInput = Array.prototype.map
           .call(new Uint8Array(hash), (x) => ("00" + x.toString(16)).slice(-2))
@@ -25,12 +25,6 @@ document.addEventListener("DOMContentLoaded", () => {
         }
         submitButton.insertAdjacentElement("afterend", p);
       });
-  });
-
-  document.getElementById("password-input").addEventListener("input", (e) => {
-    document.getElementById("password-input").value = document
-      .getElementById("password-input")
-      .value.replace(/[^0-9]/g, "");
   });
 
   document.getElementById("password-input").addEventListener("keydown", (e) => {
